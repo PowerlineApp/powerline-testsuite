156f10b35eb1ef40cd3f986f5d595be318614cdb
Austin Papp <austin.papp@gmail.com>
2016-03-12 15:22:05 -0500

    added mailgun

--- powerlineusertest.py
+++ powerlineusertest.py
@@ -11,10 +11,19 @@ import powerline
 import unittest
 import ConfigParser
 import os
+import sys
+import mailgun
 
-PLCFG = "{path:s}/powerline.cfg".format(path=os.getcwd())
-USER_SEC = 'user'
-MYSQL_SEC = 'mysql'
+try:
+    import requests
+except ImportError:
+    print "requests mod is needed to use the powerline user testsuite "
+    sys.exit(1)
+
+PLCFG       = os.path.join( os.getcwd(), "powerline.cfg")
+USER_SEC    = "user"
+MYSQL_SEC   = "mysql"
+MAILGUN_SEC = "mailgun"
 
 class PowerlineUserTests(unittest.TestCase):
 
@@ -29,20 +38,30 @@ class PowerlineUserTests(unittest.TestCase):
     mydbname = None
     myhost   = None
 
+    mailgun  = None
+
     @classmethod
     def setUpClass(cls):
         """ Pre class execution """
         cls.config = ConfigParser.ConfigParser()
         cls.config.read(PLCFG)
-        cls.username = cls.config.get(USER_SEC, 'username')
-        cls.password = cls.config.get(USER_SEC, 'password')
-        cls.url = cls.config.get(USER_SEC, 'url')
+
+        cls.username    = cls.config.get(USER_SEC, 'username')
+        cls.password    = cls.config.get(USER_SEC, 'password')
+        cls.url         = cls.config.get(USER_SEC, 'url')
 
         if cls.config.has_section(MYSQL_SEC):
-            cls.myuser = cls.config.get(MYSQL_SEC, 'user')
-            cls.mypass = cls.config.get(MYSQL_SEC, 'password')
-            cls.mydbname = cls.config.get(MYSQL_SEC, 'dbname')
-            cls.myhost = cls.config.get(MYSQL_SEC, 'host')
+            cls.myuser      = cls.config.get(MYSQL_SEC, 'user')
+            cls.mypass      = cls.config.get(MYSQL_SEC, 'password')
+            cls.mydbname    = cls.config.get(MYSQL_SEC, 'dbname')
+            cls.myhost      = cls.config.get(MYSQL_SEC, 'host')
+
+        if cls.config.has_section(MAILGUN_SEC):
+            cls.mg_key  = cls.config.get(MAILGUN_SEC, 'private')
+            cls.mailgun = mailgun.MailGunApi(cls.mg_key)
 
         cls.api = powerline.powerline(cls.username, cls.password, cls.url)
         cls.api.login()
+
+
+
